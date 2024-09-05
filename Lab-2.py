c2425b48929090a1ada596056b57cad9539abdb5
Zoe Ohana <zoeomyers@gmail.com>
2024-09-05 12:56:26 -0700

    finished lab 2!

---
+++ Lab-2.py
@@ -0,0 +1,30 @@
+name = "Zoe Myers"
+print(name)
+age = 20
+print(age)
+height = 5.25
+print(height)
+favorite_color = "Pink"
+print(favorite_color)
+print(name, age, height, favorite_color)
+print(f"Hello, my name is {name}, I am {age}, {height} ft tall, and my favorite color is {favorite_color}!")
+print(f"""
+Name: {name}
+Age: {age}
+Height: {height}
+Favorite Color: {favorite_color}""")
+import math
+circle_area = math.pi * 5 ** 2
+print(round(circle_area, 1))
+print(math.sqrt(age))
+print(math.sin(height))
+print(math.cos(height))
+print(age + 5)
+print(height - 4)
+print(age * height)
+print(height / 2)
+print(age % 3)
+print(age ** 2)
+fahrenheit = int(input())
+celsius = (fahrenheit - 32) * (5/9)
+print(celsius)
